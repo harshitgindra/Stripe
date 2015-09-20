d652a6dd1205eafee40dee80ee29f8b2cf5b8e8c
Harshit Gindra <harshitgindra@gmail.com>
2015-09-20 18:06:28 -0500

    Added DB Connection and Classes

--- Stripe/Controllers/HomeController.cs
+++ Stripe/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Stripe.Models;
 
 namespace Stripe.Controllers
 {
@@ -10,7 +11,65 @@ namespace Stripe.Controllers
     {
         public ActionResult Index()
         {
+            using (var context = new StripeEntities())
+            {
+                ViewBag.UserTypeList = getUserTypeList(context);
+            }
             return View();
+        }
+
+        public List<SelectListItem> getUserTypeList(StripeEntities context)
+        {
+            List<SelectListItem> userTypeList = new List<SelectListItem>();
+            userTypeList.Add(new SelectListItem() { Text = "Please select User Type", Value = "default" });
+            var result = context.User_Type.ToList();
+            foreach (var item in result)
+            {
+                userTypeList.Add(new SelectListItem() { Text = item.userType_Name, Value = item.userType_ID });
+            }
+            return userTypeList;
+        }
+
+        [HttpPost]
+        public ActionResult Login(Login login)
+        {
+            using (var context = new StripeEntities())
+            {
+                var loginCheck = context.Logins.Where(check => check.login_username == login.login_username && check.login_password == login.login_password).Single();
+                if (loginCheck == null)
+                {
+                    return View("Error");
+                }
+                else
+                {
+                    switch (loginCheck.User_Type_userType_ID)
+                    {
+                        case "R":
+                            return RedirectToAction("Home", "Refree", new { id = loginCheck.login_ID });
+                            break;
+                        case "S":
+                            return RedirectToAction("Home", "School", new { id = loginCheck.login_ID });
+                            break;
+                        case "A":
+                            return RedirectToAction("Home", "Admin", new { id = loginCheck.login_ID });
+                            break;
+                        default:
+                            return RedirectToAction("Index", "Home");
+                            break;
+                    }
+                }
+            }
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(Login login)
+        {
+            using (var context = new StripeEntities())
+            {
+                context.Logins.Add(login);
+                context.SaveChanges();
+                return View();
+            }
         }
 
         public ActionResult About()
---
+++ Stripe/Controllers/RefereeController.cs
@@ -0,0 +1,24 @@
+﻿using Stripe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Stripe.Controllers
+{
+    public class RefereeController : Controller
+    {
+        //
+        // GET: /Referee/
+        public ActionResult Home(int id)
+        {
+            using (var context = new StripeEntities())
+            {
+                var RefereeProfile = context.User_Profile.Where(loginid => loginid.Login_login_ID == id).SingleOrDefault();
+                 return View("Home", RefereeProfile);
+            }
+           
+        }
+	}
+}
